674f1be4efe6d2a9e4b9f1f28a2dfe75138ce16f
Bryan J Kariuki <webdeveloperjoekariuki@gmail.com>
2024-12-22 09:55:51 -0800

    basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+num1 =10
+num2 =5
+
+addition =num1 + num2
+subtraction =num1 - num2
+multiplication =num1 * num2
+
+print(f"addition of {num1} and {num2} is {addition}")
+print(f"subtraction of {num1} and {num2} is {subtraction}")
+print(f"multiplication of {num1} and {num2} is {multiplication}")
